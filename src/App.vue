7aaaa5512bd130a41f4856c40770cf527999fd4c
Ana <Ana>
2024-10-22 20:01:23 +0200

    buscador funcional

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { reactive, ref } from 'vue';
+import { reactive, ref, computed } from 'vue';
 import Form from './components/form.vue'
 
 const arrayUsers = reactive([]);
@@ -8,18 +8,22 @@ const gestioUsers = (datosRecibidos) => {
   arrayUsers.push(datosRecibidos);
 }
 
+// buscador
+const buscador = ref('');
+const arrayFiltrado = computed(() =>
+  arrayUsers.filter(user => user.nom.toLowerCase().includes(buscador.value))
+)
+
 </script>
 
 <template>
   <h1>Llibreta de contactes</h1>
   <main>
     <Form @addContact="gestioUsers"></Form>
+    <hr>
+    <input type="text" placeholder="Busca..." v-model="buscador"/>
     <ul>
-      <li v-for = 'user in arrayUsers' :key ='user.nom'>{{ user.nom }} - {{ user.telf }}</li>
+      <li v-for = 'user in arrayFiltrado' :key ='user.nom'>{{ user.nom }} - {{ user.telf }}</li>
     </ul>
   </main>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/form.vue
+++ src/components/form.vue
@@ -7,16 +7,16 @@ const inputNumero = ref();
 
 const emit = defineEmits(['addContact']);
 
-
-const addContact = () =>{
+// enviar datos a la función addContact
+const addContact = () => {
     emit('addContact', {nom: inputNom.value, telf: inputNumero.value});
 }
+
 </script>
 
 <template>
     <input type="text" placeholder="Nom del contacte" id="nameContact" v-model="inputNom" required>
     <input type="number" placeholder="Número del contacte" v-model="inputNumero" required>
     <button @click="addContact">Afegir contacte</button>
-    <hr>
-    <input type="text" placeholder="Busca..." v-model="buscador"/>
+
 </template>
