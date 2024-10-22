efea4f23e5e77691a3ed8e2f48c2c81cfc152707
Ana <Ana>
2024-10-22 19:22:48 +0200

    Afegir contactes funciona

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { reactive, ref } from 'vue';
+import Form from './components/form.vue'
+
+const arrayUsers = reactive([]);
+
+const gestioUsers = (datosRecibidos) => {
+  arrayUsers.push(datosRecibidos);
+}
+
+</script>
+
+<template>
+  <h1>Llibreta de contactes</h1>
+  <main>
+    <Form @addContact="gestioUsers"></Form>
+    <ul>
+      <li v-for = 'user in arrayUsers' :key ='user.nom'>{{ user.nom }} - {{ user.telf }}</li>
+    </ul>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/form.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+// codigo
+const inputNom = ref();
+const inputNumero = ref();
+
+const emit = defineEmits(['addContact']);
+
+
+const addContact = () =>{
+    emit('addContact', {nom: inputNom.value, telf: inputNumero.value});
+}
+</script>
+
+<template>
+    <input type="text" placeholder="Nom del contacte" id="nameContact" v-model="inputNom">
+    <input type="number" placeholder="Número del contacte" v-model="inputNumero">
+    <button @click="addContact">Afegir contacte</button>
+    <hr>
+    <input type="text" placeholder="Busca..." v-model="buscador"/>
+</template>
